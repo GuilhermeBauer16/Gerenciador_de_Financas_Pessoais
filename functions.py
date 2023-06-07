d0037f8d869bb1464713acb65093d50a2cb4238c
Guilherme <guilhermebauer16@gmail.com>
2023-06-07 18:11:59 -0300

    Add funções receitas e dispesas

--- functions.py
+++ functions.py
@@ -31,3 +31,71 @@ def receitas(msg):
             print('Por favor digite um valor.Exemplo: 100,00')    
             
     return float_receitas
+
+
+def receita():
+    from time import sleep
+    fonte = ''
+    while True:
+        cabecalho(f'Tipo de receita', 40)
+        print('''
+[1]Salário
+[2]Freelancer
+[3]Outro 
+                ''')
+        sleep(1)
+        print('/=' * 40)
+        op_receita = conversor_numero('sua opção: ', int)
+        print('/=' * 40)
+        if op_receita == 1:
+            fonte ='salario'
+            break
+        elif op_receita == 2:
+            fonte = 'freelancer'
+            break
+        elif op_receita == 3:
+            fonte = 'outro'
+            break
+
+        else:
+            print('por favor imforme uma opção valida!')
+
+    return fonte
+
+
+def dispesa():
+    from time import sleep
+    fonte = ''
+    while True:
+        cabecalho(f'Tipo de Dispesa', 40)
+        print('''
+[1]Esenssial 
+[2]Lazer 
+[3]Financiameto
+[4]Viagem
+[5]Outro 
+                ''')
+        sleep(1)
+        print('/=' * 40)
+        op_receita = conversor_numero('sua opção: ', int)
+        print('/=' * 40)
+        if op_receita == 1:
+            fonte ='Esenssial'
+            break
+        elif op_receita == 2:
+            fonte = 'Lazer'
+            break
+        elif op_receita == 3:
+            fonte = 'Financiameto'
+            break
+        elif op_receita == 4:
+            fonte = 'Viagem'
+            break
+        elif op_receita == 5:
+            fonte = 'outro'
+            break
+
+        else:
+            print('por favor imforme uma opção valida!')
+
+    return fonte
--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import  _sqlite3
 import functions
+from time import sleep
 banco = _sqlite3.connect("Gerenciador_Tarefas.db")
 cursor = banco.cursor()
+fonte = ''
+tipo = ''
+cursor.execute("SELECT name FROM sqlite_master WHERE type ='table' AND name='geral' ")
+tabela_existe = cursor.fetchone()
+
+if not tabela_existe:
+
+    cursor.execute("CREATE TABLE geral ('valor' real , 'mes' text , 'ano' integer , 'opcao' text ,'comentario' text , 'tipo' text)")
+
+
 
 while True:
     functions.cabecalho('Gerenciador de tarefas' , 40)
@@ -15,7 +26,21 @@ while True:
     ''')
     print('/=' * 40)
     opcao = functions.conversor_numero('Sua opção: ',int)
-    if opcao == 1:
-        functions.cabecalho('Receitas' , 40)
-        receita = functions.receitas('Suas receitas.R$ ')
+    if opcao == 1 or opcao == 2:
+        if opcao == 1:  
+            tipo = 'Receita'
+            fonte = functions.receita()
+            
+        elif opcao == 2 :
+            tipo = "Dispesa"
+            fonte = functions.dispesa()
+
+        functions.cabecalho(f'{tipo}' , 40)
+        receita = functions.receitas(f'Sua {tipo}.R$ ')
+        mes = str(input('Imforme o mes por extenso: ')).upper()
+        ano = functions.conversor_numero('Imforme o ano: ' , int)
+        comentario = str(input('Comentario: '))
+        cursor.execute("INSERT INTO geral VALUES(?,?,?,?,?,?)",(receita, mes, ano ,fonte ,comentario ,tipo))
+        banco.commit()
+
         
